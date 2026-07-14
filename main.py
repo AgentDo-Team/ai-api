b2f0d38fb0ecd4af4f870d67cec4d3dc4ca9b9e6
junggayeong <152073691+junggayeong@users.noreply.github.com>
2026-07-14 12:13:02 +0900

    Feat: 프론트엔드 CORS 설정

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.auth import router as auth_router
 from app.api.profile import router as profile_router
 from app.common.exception_handlers import register_exception_handlers
 from app.schemas.response import ApiResponse
+
+
 
 app = FastAPI(title="ai-api")
 register_exception_handlers(app)
 app.include_router(auth_router)
 app.include_router(profile_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # 프론트 주소
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root() -> ApiResponse[dict]:
